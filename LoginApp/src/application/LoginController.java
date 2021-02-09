d9e269ff36bd2e1d7c7a92a759dcf6cf4d10c08a
mrhpn <mr.htetphyonaing@gmail.com>
2021-02-09 17:12:19 +0630

    Login App - Added next page

--- LoginApp/src/application/LoginController.java
+++ LoginApp/src/application/LoginController.java
@@ -1,12 +1,21 @@
 package application;
 
+import java.io.IOException;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
 
 public class LoginController {
 
@@ -14,7 +23,7 @@ public class LoginController {
 	private TextField tfEmail;
 
 	@FXML
-	private PasswordField tfPassword;
+	private PasswordField pfPassword;
 
 	@FXML
 	private RadioButton rbMale;
@@ -29,15 +38,46 @@ public class LoginController {
 	private Label lblStatus;
 
 	@FXML
-	void processLogin(ActionEvent event) {
+	private ComboBox<String> cobRole;
+
+	private String gender;
+
+	@FXML
+	void processGender(ActionEvent event) {
+		if (rbMale.isSelected()) {
+			this.gender = rbMale.getText();
+		}
+
+		if (rbFemale.isSelected()) {
+			this.gender = rbFemale.getText();
+		}
+	}
+
+	@FXML
+	void processLogin(ActionEvent event) throws IOException {
+
 		lblStatus.setVisible(true);
 
-		System.out.println(tfEmail.getText());
+		if (tfEmail.getText().trim().equals("admin") && pfPassword.getText().equals("11111")) {
 
-		if (tfEmail.getText().trim().equals("admin@gmail.com") && tfPassword.getText().equals("aaaaa"))
-			lblStatus.setText("Login success!");
-		else
-			lblStatus.setText("Login fail!");
+			lblStatus.setTextFill(Color.GREEN);
+			lblStatus.setText("Login Success");
+
+			User.setEmail(tfEmail.getText());
+			User.setRole(cobRole.getValue());
+			User.setGender(this.gender);
+
+			Stage mainStage = new Stage();
+			BorderPane mainParent = (BorderPane) FXMLLoader.load(getClass().getResource("../main/MainUI.fxml"));
+			Scene scene = new Scene(mainParent);
+			mainStage.setScene(scene);
+			mainStage.show();
+			mainStage.setTitle("MainUI");
+		} else {
+			lblStatus.setTextFill(Color.RED);
+			lblStatus.setText("Incorrect email or password");
+		}
+
 	}
 
 }
---
+++ LoginApp/src/application/User.java
@@ -0,0 +1,38 @@
+package application;
+
+public class User {
+	
+	private static String email;
+	
+	private static String role;
+	
+	private static String gender;
+
+	public static String getEmail() {
+		return email;
+	}
+
+	public static void setEmail(String email) {
+		User.email = email;
+	}
+
+	public static String getRole() {
+		return role;
+	}
+
+	public static void setRole(String role) {
+		User.role = role;
+	}
+
+	public static String getGender() {
+		return gender;
+	}
+
+	public static void setGender(String gender) {
+		User.gender = gender;
+	}
+	
+	
+	
+
+}
